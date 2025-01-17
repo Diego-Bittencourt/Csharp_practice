2102f9bf8b95017f4f4545708f23059e3f33b056
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2025-01-17 17:58:31 +0900

    add simple console program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Press a number");
+
+            string userInput = Console.ReadLine();
+            int userNumber;
+
+            try
+            {
+                int isItNumeric = int.Parse(userInput);
+                userNumber = isItNumeric;
+
+            }
+            catch (Exception ex) 
+            { 
+                userNumber = 0;
+            }
+
+            Console.WriteLine($"The input number is: {userNumber}");
+
+        }
+    }
+}
